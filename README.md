0be71ca79438239e1e5897df4bc264e95a4d9188
Tejesh Mehta <tejesh.mehta@gmail.com>
2015-03-05 16:17:18 -0800

    initial implementation

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # substream-pump
 because primus substreams don't have pipes :(
+
+##Usage
+
+```js
+var pump = require('substream-pump');
+
+// callback is optional for errors or on readable close
+pump(streamOrSubstreamSource, substreamDest, cb);
+```
+
+##License
+MIT
---
+++ index.js
@@ -0,0 +1,40 @@
+'use strict';
+
+module.exports = pump;
+
+function pump (readStream, writeStream, callback) {
+  var callbackCalled = false;
+
+  function call(a, b, c) {
+    if (callback && !callbackCalled) {
+      callback(a, b, c);
+      callbackCalled = true;
+    }
+  }
+
+  readStream.addListener('data', function(chunk) {
+    if (writeStream.write(chunk) === false) readStream.pause();
+  });
+
+  writeStream.addListener('drain', function() {
+    readStream.resume();
+  });
+
+  readStream.addListener('end', function() {
+    writeStream.end();
+  });
+
+  readStream.addListener('close', function() {
+    call();
+  });
+
+  readStream.addListener('error', function(err) {
+    writeStream.end();
+    call(err);
+  });
+
+  writeStream.addListener('error', function(err) {
+    readStream.destroy();
+    call(err);
+  });
+}
